5683572836368cc3866eac7735150892f179f106
V1le <37912520+V1leDev@users.noreply.github.com>
2019-12-07 21:40:33 +0100

    Added comments to deletion_calculator.py

--- Database Management/next_step_calcer.py
+++
@@ -1,9 +0,0 @@
-import math
-
-goal = 432000
-current_value = float(input("Input startvalue\n"))
-while current_value != goal:
-    next_value = math.ceil(-current_value / (-current_value + goal))
-    current_value = current_value - (1 / next_value) * current_value
-    current_value = round(current_value, 4)
-    print("To get to", current_value, "input", next_value)
