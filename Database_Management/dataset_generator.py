d9a59fd7655cdc842142fa4174cf086fd08fbbfa
V1le <37912520+V1leDev@users.noreply.github.com>
2019-12-15 15:44:30 +0100

    Added dataset_generator.py

---
+++ Database_Management/dataset_generator.py
@@ -0,0 +1,59 @@
+import csv
+import operator
+import os
+
+# deleting all unneeded columns
+with open('source.csv', 'rt') as inp, open('first_edit.csv', 'wt', newline='') as out:
+    writer = csv.writer(out)
+    reader = csv.reader(inp)
+    for column in reader:
+        writer.writerow((column[0], column[1], column[4]))
+    inp.close()
+    out.close()
+
+# deleting all rows not containing XBTUSD values
+with open('first_edit.csv', 'rt') as inp, open('second_edit.csv', 'wt', newline='') as out:
+    writer = csv.writer(out)
+    reader = csv.reader(inp)
+    for row in reader:
+        if row[1] == "XBTUSD":
+            writer.writerow(row)
+    inp.close()
+    out.close()
+
+# deleting symbol column
+with open('second_edit.csv', 'rt') as inp, open('third_edit.csv', 'wt', newline='') as out:
+    writer = csv.writer(out)
+    reader = csv.reader(inp)
+    for column in reader:
+        writer.writerow((column[0], column[2]))
+    inp.close()
+    out.close()
+
+# sorting list by timestamp
+with open('third_edit.csv', 'rt') as inp, open('sorted.csv', 'wt', newline='') as out:
+    writer = csv.writer(out)
+    reader = csv.reader(inp)
+    sortedlist = sorted(reader, key=operator.itemgetter(0))
+    for row in sortedlist:
+        writer.writerow(row)
+
+# finalizing list with one value per second
+with open('sorted.csv', 'rt') as inp, open('final.csv', 'wt', newline='') as out:
+    writer = csv.writer(out)
+    reader = csv.reader(inp)
+    current = 0
+    for row in reader:
+        var = row[0]
+        sliced = var[17:19]
+        if current != sliced:
+            writer.writerow(row)
+            current = sliced
+    inp.close()
+    out.close()
+
+# removing unneeded temporary files
+os.remove('first_edit.csv')
+os.remove('second_edit.csv')
+os.remove('third_edit.csv')
+os.remove('sorted.csv')
