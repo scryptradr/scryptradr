99eb3cb26bbbc5f2d6acf32b3b4a81d6edf65578
Fr4ctal-Dev <plankermatteo23@gmail.com>
2019-12-30 22:07:32 +0100

    reworked Engine for multithreading

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+import threading
+
+from Engine import Worker
+
 TRAIN = True
 
+if __name__ == "__main__":
+    while True:
+        # Mainloop
+        if TRAIN:
+            # program in train mode
+            pass
+            # todo get input and output values from db
+            # todo calculate percentages from values
+            # todo put output percentages in sigmoid function
+            # todo train NN with percentages (input) and sigmoid numbers (output)
 
-while True:
-    # Mainloop
-    if TRAIN:
-        # program in train mode
-        pass
-        # todo get input and output values from db
-        # todo calculate percentages from values
-        # todo put output percentages in sigmoid function
-        # todo train NN with percentages (input) and sigmoid numbers (output)
+        else:
+            # program is live
 
-    else:
-        # program is live
-        pass
-        # todo shift current data by one and append pulled current price (converted to percent) from API
-        # todo plug current data into nn
-        # todo send resulting data from nn to the engine
+            # start queue thread
+            t_queue = threading.Thread(target=Worker.order_worker_thread())
+            t_queue.start()
